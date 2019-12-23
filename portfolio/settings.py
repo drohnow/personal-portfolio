525a250b8039102e0c3585a35812fd12ff6fcc71
David <davidrohnow@gmail.com>
2019-12-23 03:59:40 -0800

    add jobs app

--- portfolio/settings.py
+++ portfolio/settings.py
@@ -31,6 +31,7 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    'jobs.apps.JobConfig',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
@@ -74,11 +75,16 @@ WSGI_APPLICATION = 'portfolio.wsgi.application'
 # https://docs.djangoproject.com/en/2.2/ref/settings/#databases
 
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
-    }
-}
+	   'default': {
+	      'ENGINE': 'django.db.backends.postgresql',
+	      'NAME': 'portfoliodb',
+	      'USER':'postgres',
+	      'PASSWORD':'Trek1515',
+	      'HOST':'localhost',
+	      'PORT':'5432',
+	    }
+	}
+
 
 
 # Password validation
@@ -118,3 +124,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/2.2/howto/static-files/
 
 STATIC_URL = '/static/'
+
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+
+MEDIA_URL = '/media/'
